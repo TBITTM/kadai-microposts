6155d9cd237dbd41f37242563a6abf6962bab334
MTOBITT <tomoetbitt@gmail.com>
2021-12-14 21:31:46 +0000

    first commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,9 @@ class User < ApplicationRecord
   has_many :followings, through: :relationships, source: :follow
   has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
   has_many :followers, through: :reverses_of_relationship, source: :user
+  has_many :favorites
+  has_many :likes, through: :favorites, source: :micropost
+  has_many :reverses_of_favorite, class_name: 'Favorite', foreign_key: 'micropost_id'
   
   def follow(other_user)
     unless self == other_user
@@ -31,4 +34,20 @@ class User < ApplicationRecord
     Micropost.where(user_id: self.following_ids + [self.id])
   end
 
+
+
+  def like(other_user)
+    unless self == other_user
+      self.favorites.find_or_create_by(micropost_id: other_user.id)
+    end
+  end
+
+  def unlike(other_user)
+    favorite = self.favorites.find_by(micropost_id: other_user.id)
+    favorite.destroy if favorite
+  end
+
+  def liking?(other_user)
+    self.likes.include?(other_user)
+  end
 end
